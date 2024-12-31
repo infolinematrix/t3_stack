d623a31721c687ed6caf278a7830c1eecf15b198
Subha Sundar Das <infolinematrix@gmail.com>
2024-12-31 16:51:29 +0530

    Auth Module

---
+++ src/app/(auth)/_components/signin-goole-button.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
+import { Icons } from "@/app/(app)/_components/icons";
+import { Button } from "@/components/ui/button";
+
+export default function SigninGoogleButton() {
+  const handleSignIn = async () => {
+    try {
+      await signIn("google", { callbackUrl: "/" });
+    } catch (error) {
+      console.error("Sign in failed:", error);
+
+      alert("Sign-in failed, please try again.");
+    }
+  };
+  return (
+    <Button
+      type="button"
+      variant="outline"
+      className="w-full py-5"
+      onClick={handleSignIn}
+    >
+      <Icons.google className="mr-2 h-4 w-4" />
+      Login with Google
+    </Button>
+  );
+}
